6052970cb57c269c773ddce46033c135e35b7677
黃庭昱 <devil60612@gmail.com>
2018-10-05 17:25:58 +0800

    complete Ch1

---
+++ Ch1_Lab1/source/Main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	puts("Welcome to C!");
+	system("pause");
+	return 0;
+}
---
+++ Ch1_Lab2/source/User_defined.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int User_defined_print()
+{
+	puts("Welcome to C!");
+}
